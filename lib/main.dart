14f07bf480aaa5cdcd5c1d2108e02aec849bac73
Mohammad.slm72 <Mohammad.slm72@gmail.com>
2023-07-03 17:10:23 +0330

    api call

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/Model/PageViewModel.dart';
+import 'package:dio/dio.dart';
+import 'package:progress_indicators/progress_indicators.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -7,9 +10,24 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  late Future<List<PageViewModel>> pageViewFuture;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    pageViewFuture = SendRequestPageView();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +41,48 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         color: Colors.purple,
+        child: Column(
+          children: [
+            Container(
+              height: 250,
+              color: Colors.white,
+              child: FutureBuilder<List<PageViewModel>>(
+                future: pageViewFuture,
+                builder: (context , snapshot){
+                  if(snapshot.hasData){
+                    return Container(
+                      height: 200,
+                      color: Colors.red,
+
+                    );
+                  }else{
+                    return Center(
+                      child: JumpingDotsProgressIndicator(
+                        fontSize: 60,
+                        dotSpacing: 5,
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  Future<List<PageViewModel>> SendRequestPageView() async{
+    List<PageViewModel> model = [];
+
+    var response = await Dio().get("http://mohammad.slm72.freehost.io/db.json");
+    
+    print(response);
+
+    for(var item in response.data['photos']){
+      model.add(PageViewModel(item['id'], item['imgUrl']));
+    }
+
+    return model;
+}
 }
